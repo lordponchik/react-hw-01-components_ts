5fea8980b02b65d6c020e86da84b2a28a32a7a21
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-09-18 19:05:45 +0200

    add Profile in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import './App.css';
+import userData from './data/user.json';
+
+import Profile from './components/Profile/Profile';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
 }
 
 export default App;
